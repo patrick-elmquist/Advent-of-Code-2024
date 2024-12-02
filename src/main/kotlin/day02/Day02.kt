f0adac637b8c1b6751c7b0226be83292be65c7b8
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2024-12-02 07:09:04 +0100

    Day 2 done

--- src/main/kotlin/day02/Day02.kt
+++ src/main/kotlin/day02/Day02.kt
@@ -1,26 +1,46 @@
 package day02
 
+import common.Input
 import common.day
+import kotlin.math.abs
 
 // answer #1:
-// answer #2:
+// answer #2: not 307, not 259
 
 fun main() {
     day(n = 2) {
         part1 { input ->
-
+            parseReports(input).count { report -> report.checkLevels() }
         }
         verify {
-            expect result null
-            run test 1 expect Unit
+            expect result 213
+            run test 1 expect 2
         }
 
         part2 { input ->
-
+            parseReports(input)
+                .count { report -> report.checkLevels() || report.tryBruteForce() }
         }
         verify {
-            expect result null
-            run test 1 expect Unit
+            expect result 285
+            run test 1 expect 4
         }
     }
 }
+
+private fun parseReports(input: Input) = input.lines
+    .map { line -> line.split(" ").map(String::toInt) }
+
+private fun List<Int>.checkLevels(): Boolean =
+    checkLevels { a, b -> a < b } || checkLevels { a, b -> a > b }
+
+private fun List<Int>.checkLevels(condition: (a: Int, b: Int) -> Boolean) =
+    zipWithNext().all { (a, b) -> condition(a, b) && abs(a - b) in (1..3) }
+
+private fun List<Int>.tryBruteForce() =
+    indices.any { index ->
+        toMutableList()
+            .apply { removeAt(index) }
+            .checkLevels()
+    }
+
