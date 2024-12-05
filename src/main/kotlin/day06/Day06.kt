0deb00d1394cd4c1a5c9fb443c9c1b97f7a8662e
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2024-12-05 09:43:44 +0100

    Prepare for Day 6

---
+++ src/main/kotlin/day06/Day06.kt
@@ -0,0 +1,26 @@
+package day06
+
+import common.day
+
+// answer #1:
+// answer #2:
+
+fun main() {
+    day(n = 6) {
+        part1 { input ->
+
+        }
+        verify {
+            expect result null
+            run test 1 expect Unit
+        }
+
+        part2 { input ->
+
+        }
+        verify {
+            expect result null
+            run test 1 expect Unit
+        }
+    }
+}
