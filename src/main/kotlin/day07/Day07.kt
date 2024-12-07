5a898b0fe3c09d86a6a25167de1584f128b25156
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2024-12-07 08:37:05 +0100

    Day 7 done

--- src/main/kotlin/day07/Day07.kt
+++ src/main/kotlin/day07/Day07.kt
@@ -1,26 +1,56 @@
 package day07
 
+import common.Input
 import common.day
 
-// answer #1:
-// answer #2:
+// answer #1: 303766880536
+// answer #2: 337041851384440
 
 fun main() {
     day(n = 7) {
         part1 { input ->
-
+            parseInputToExpectedAndValues(input)
+                .filter { (result, values) ->
+                    solve(result, values, useCombine = false)
+                }
+                .sumOf { (expected, _) -> expected }
         }
         verify {
-            expect result null
-            run test 1 expect Unit
+            expect result 303766880536L
+            run test 1 expect 3749L
         }
 
         part2 { input ->
-
+            parseInputToExpectedAndValues(input)
+                .filter { (result, values) ->
+                    solve(result, values, useCombine = true)
+                }
+                .sumOf { (expected, _) -> expected }
         }
         verify {
-            expect result null
-            run test 1 expect Unit
+            expect result 337041851384440L
+            run test 1 expect 11387L
         }
     }
 }
+
+private fun solve(expected: Long, values: List<Long>, useCombine: Boolean): Boolean =
+    evaluate(expected, values.first(), values.drop(1), useCombine)
+
+private fun evaluate(expected: Long, current: Long, values: List<Long>, useCombine: Boolean = false): Boolean {
+    if (values.isEmpty()) return current == expected
+
+    val value = values.first()
+    val remaining = values.drop(1)
+
+    if (evaluate(expected, current + value, remaining, useCombine)) return true
+    if (evaluate(expected, current * value, remaining, useCombine)) return true
+    return useCombine && evaluate(expected, "$current$value".toLong(), remaining, useCombine)
+}
+
+private fun parseInputToExpectedAndValues(input: Input): List<Pair<Long, List<Long>>> =
+    input.lines.map { line ->
+        line.split(": ").let { (result, values) ->
+            result.toLong() to values.split(" ").map(String::toLong)
+        }
+    }
