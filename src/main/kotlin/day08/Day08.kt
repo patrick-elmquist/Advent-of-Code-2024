489375ac8b2c8962cd8bb9e5fd8870b4a79438d0
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2024-12-07 08:44:54 +0100

    Prepare for Day 8

---
+++ src/main/kotlin/day08/Day08.kt
@@ -0,0 +1,26 @@
+package day08
+
+import common.day
+
+// answer #1:
+// answer #2:
+
+fun main() {
+    day(n = 8) {
+        part1 { input ->
+
+        }
+        verify {
+            expect result null
+            run test 1 expect Unit
+        }
+
+        part2 { input ->
+
+        }
+        verify {
+            expect result null
+            run test 1 expect Unit
+        }
+    }
+}
