377eafbb12080bccdc4c4bf313493e0d0428f2c6
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2024-12-08 08:43:59 +0100

    Day 8 cleanup, stil dirty

--- src/main/kotlin/day08/Day08.kt
+++ src/main/kotlin/day08/Day08.kt
@@ -3,7 +3,6 @@ package day08
 import common.day
 import common.grid
 import common.util.Point
-import common.util.log
 import common.util.print
 
 // answer #1: 308
@@ -13,28 +12,17 @@ fun main() {
     day(n = 8) {
         part1 { input ->
             val grid = input.grid
-            val antennas = grid
-                .filter { it.value != '.' }
-                .entries
-                .groupBy { entry -> entry.value }
-                .map { entry -> entry.key to entry.value.map { entry -> entry.key } }
-
-            println(antennas)
+            val antennas = parseAntennasByType(grid)
 
             val p = mutableSetOf<Point>()
-            antennas.map { (type, points) ->
+            antennas.map { (_, points) ->
                 points.forEach { p1 ->
                     points.filter { it != p1  }
                         .forEach { p2 ->
-                            val diffX = Point(1 * (p1.x - p2.x), 1 * (p1.y - p2.y))
-                            val diffY = Point(1 * (p2.x - p1.x), 1 * (p2.y - p1.y))
-
-                            val newPoint1 = p1 + diffX
-                            val newPoint2 = p2 + diffY
-
-                            "p1:$p1 p2:$p2 adding points $newPoint1 $newPoint2".log()
-                            p.add(newPoint1)
-                            p.add(newPoint2)
+                            val diffX = Point(p1.x - p2.x, p1.y - p2.y)
+                            val diffY = Point(p2.x - p1.x, p2.y - p1.y)
+                            p.add(p1 + diffX)
+                            p.add(p2 + diffY)
                         }
                 }
             }
@@ -48,45 +36,40 @@ fun main() {
 
         part2 { input ->
             val grid = input.grid
-            val antennas = grid
-                .filter { it.value != '.' }
-                .entries
-                .groupBy { entry -> entry.value }
-                .map { entry -> entry.key to entry.value.map { entry -> entry.key } }
-
-            println(antennas)
+            val antennas = parseAntennasByType(grid)
 
             val p = mutableSetOf<Point>()
-            antennas.map { (type, points) ->
+            antennas.map { (_, points) ->
                 points.forEach { p1 ->
                     points.filter { it != p1  }
                         .forEach { p2 ->
-                            val diffX = Point(1 * (p1.x - p2.x), 1 * (p1.y - p2.y))
-                            val diffY = Point(1 * (p2.x - p1.x), 1 * (p2.y - p1.y))
+                            val diffX = Point(p1.x - p2.x, p1.y - p2.y)
 
                             var newPoint1 = p1 + diffX
                             while (newPoint1 in grid) {
                                 p.add(newPoint1)
-                                newPoint1 = newPoint1 + diffX
+                                newPoint1 += diffX
                             }
                             var newPoint2 = p1 - diffX
                             while (newPoint2 in grid) {
                                 p.add(newPoint2)
-                                newPoint2 = newPoint2 - diffX
+                                newPoint2 -= diffX
                             }
                         }
                 }
             }
 
-            println(p)
-            grid.print { t, c -> if (t in p && c == '.') '#' else c }
-
             p.count { point -> point in grid.keys }
         }
         verify {
-//            breakAfterTest()
             expect result 1147
             run test 1 expect 34
         }
     }
 }
+
+private fun parseAntennasByType(grid: Map<Point, Char>): List<Pair<Char, List<Point>>> = grid
+    .filter { it.value != '.' }
+    .entries
+    .groupBy { entry -> entry.value }
+    .map { entry -> entry.key to entry.value.map { entry -> entry.key } }
