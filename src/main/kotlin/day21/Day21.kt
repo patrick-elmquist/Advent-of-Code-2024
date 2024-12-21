ab6a7622c691a56bd0f6813190b353e086392ec0
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2024-12-21 15:24:43 +0100

    Day 21 Part 2 working but im not pleased

--- src/main/kotlin/day21/Day21.kt
+++ src/main/kotlin/day21/Day21.kt
@@ -2,61 +2,109 @@ package day21
 
 import common.day
 import common.util.Point
+import common.util.grid
+import common.util.gridOf
+import common.util.ignoreWhitespace
 import common.util.log
 import common.util.neighbors
 import java.util.PriorityQueue
+import kotlin.math.abs
 
 // answer #1: 184716
-// answer #2:
+// answer #2: 229403562787554
 
-private val keyToPointMap = mapOf(
-    '7' to Point(0, 0),
-    '8' to Point(1, 0),
-    '9' to Point(2, 0),
-    '4' to Point(0, 1),
-    '5' to Point(1, 1),
-    '6' to Point(2, 1),
-    '1' to Point(0, 2),
-    '2' to Point(1, 2),
-    '3' to Point(2, 2),
-    '0' to Point(1, 3),
-    'A' to Point(2, 3),
-)
-private val pointToKeyMap = keyToPointMap.map { (key, value) -> value to key }.toMap()
-private val dirToPointMap = mapOf(
-    '^' to Point(1, 0),
-    'A' to Point(2, 0),
-    '<' to Point(0, 1),
-    'v' to Point(1, 1),
-    '>' to Point(2, 1),
-)
-private val pointToDirMap = dirToPointMap.map { (key, value) -> value to key }.toMap()
+private val pointToKeyMap = gridOf("789", "456", "123", " 0A").ignoreWhitespace()
+private val keyToPointMap = pointToKeyMap.map { it.value to it.key }.toMap()
+private val pointToDirMap = gridOf(" ^A", "<v>").ignoreWhitespace()
+private val dirToPointMap = pointToDirMap.map { it.value to it.key }.toMap()
 
+private val keypadNumeric = gridOf("789", "456", "123", "X0A")
+private val keypadDirection = gridOf("X^A", "<v>")
 fun main() {
     day(n = 21) {
         part1 { input ->
+//            input.lines.sumOf { code ->
+//                code.dropLast(1).toInt() * rec(code, Type.Keypad)
+//                    .flatMap { rec(it, Type.Direction) }
+//                    .groupBy { it.length }
+//                    .minBy { it.key }
+//                    .value
+//                    .flatMap { rec(it, Type.Direction) }
+//                    .minOf { it.length }
+//            }
             input.lines.sumOf { code ->
-                code.dropLast(1).toInt() * rec(code, Type.Keypad)
-                    .flatMap { rec(it, Type.Direction) }
-                    .groupBy { it.length }
-                    .minBy { it.key }
-                    .value
-                    .flatMap { rec(it, Type.Direction) }
-                    .minOf { it.length }
+                solve(code, 4) * code.dropLast(1).toLong()
             }
         }
         verify {
-            expect result 184716
-            run test 1 expect 126384
+            expect result 184716L
+            run test 1 expect 126384L
         }
 
         part2 { input ->
-
+            input.lines.sumOf { code ->
+                solve(code, 27) * code.dropLast(1).toLong()
+            }
         }
         verify {
             expect result null
-            run test 1 expect Unit
         }
+    }
+}
+
+private fun findPosition(key: Char, keypad: Map<Char, Point>): Point =
+    keypad.getValue(key)
+
+private fun buildShortestPathsBetweenKeys(
+    key1: Char,
+    key2: Char,
+    keypad: Map<Char, Point>,
+): List<String> {
+    val (c1, r1) = findPosition(key1, keypad)
+    val (c2, r2) = findPosition(key2, keypad)
+    val gap = findPosition('X', keypad)
+    val dr = r2 - r1
+    val dc = c2 - c1
+    val rowMoves = if (dr >= 0) "v".repeat(abs(dr)) else "^".repeat(abs(dr))
+    val colMoves = if (dc >= 0) ">".repeat(abs(dc)) else "<".repeat(abs(dc))
+    return when {
+        dr == 0 && dc == 0 -> return listOf("")
+        dr == 0 -> listOf(colMoves)
+        dc == 0 -> listOf(rowMoves)
+        gap == Point(c2, r1) -> listOf(rowMoves + colMoves)
+        gap == Point(c1, r2) -> listOf(colMoves + rowMoves)
+        else -> listOf(rowMoves + colMoves, colMoves + rowMoves)
+    }
+}
+
+private fun buildSequenceOfShortestPaths(
+    seq: String,
+    keypad: Map<Char, Point>,
+): List<List<String>> {
+    val res = mutableListOf<List<String>>()
+    ("A$seq").zip(seq).forEach { (key1, key2) ->
+        res += listOf(buildShortestPathsBetweenKeys(key1, key2, keypad).map { it + "A" })
+    }
+    return res
+}
+
+private val cache = mutableMapOf<Pair<String, Int>, Long>()
+private fun solve(seq: String, depth: Int): Long {
+    return cache.getOrPut(seq to depth) {
+        if (depth == 1) return@getOrPut seq.length.toLong()
+
+        val keypad = if ("0123456789".any { it in seq }) {
+            keypadNumeric
+        } else {
+            keypadDirection
+        }.map { it.value to it.key }.toMap()
+
+        var res = 0L
+        for (shortestPaths in buildSequenceOfShortestPaths(seq, keypad)) {
+            val solved = shortestPaths.map { sp -> solve(sp, depth - 1) }
+            res += solved.min()
+        }
+        res
     }
 }
 
