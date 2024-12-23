8d23f6fce94f92d41bbeb0aa54dff215c0a038df
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2024-12-23 11:40:58 +0100

    Cleaning up

--- src/main/kotlin/day23/Day23.kt
+++ src/main/kotlin/day23/Day23.kt
@@ -1,6 +1,9 @@
 package day23
 
+import common.Input
 import common.day
+import kotlin.collections.component1
+import kotlin.collections.component2
 
 // answer #1: 1173
 // answer #2: cm,de,ez,gv,hg,iy,or,pw,qu,rs,sn,uc,wq
@@ -8,19 +11,11 @@ import common.day
 fun main() {
     day(n = 23) {
         part1 { input ->
-            val allPairs = input.lines.flatMap {
-                it.split('-').sorted().let { (a, b) -> listOf(a to b, b to a) }
-            }.distinct()
-
-            val connections = allPairs.groupBy { it.first }
-                .mapValues { (_, value) -> value.map { it.second }.toSet() }
-
-            val count = connections.keys.map { node ->
-                connections.countCyclesOfThree(node)
-            }
-
-            val counted = count.flatMap { it }.toSet().count { it.any { it.startsWith('t') } }
-            counted
+            val connections = buildConnectionsMap(input)
+            connections.keys
+                .flatMap { node -> connections.countCyclesOfThree(node) }
+                .toSet()
+                .count { it.any { it.startsWith('t') } }
         }
         verify {
             expect result 1173
@@ -28,27 +23,18 @@ fun main() {
         }
 
         part2 { input ->
-            val allPairs = input.lines.flatMap {
-                it.split('-').sorted().let { (a, b) -> listOf(a to b, b to a) }
-            }.distinct()
-
-            val connections = allPairs.groupBy { it.first }
-                .mapValues { (_, value) -> value.map { it.second }.toSet() }
-
-            val node0 = connections.entries.toList().first()
-            var cycles = mutableListOf<List<String>>()
-            connections.dfsCycle(
-                node = node0.value.first(),
-                parent = node0.key,
-                cycles = cycles,
-            )
-            val sortedCycles = cycles.map { it.sorted() }.sortedBy { it.size }
-
-            sortedCycles.filter { cycle ->
-                val s = cycle.map { connections.getValue(it) + it }
-                    .reduce { a, b -> a.intersect(b) }
-                cycle.all { it in s }
-            }.maxBy { it.size }.sorted().joinToString(",")
+            val connections = buildConnectionsMap(input)
+            connections
+                .findCycles()
+                .filter { cycle ->
+                    val intersectingNodes = cycle
+                        .map { node -> connections.getValue(node) + node }
+                        .reduce(Set<String>::intersect)
+                    cycle.all { node -> node in intersectingNodes }
+                }
+                .maxBy { it.size }
+                .sorted()
+                .joinToString(",")
         }
         verify {
             expect result "cm,de,ez,gv,hg,iy,or,pw,qu,rs,sn,uc,wq"
@@ -57,56 +43,66 @@ fun main() {
     }
 }
 
+private fun Map<String, Set<String>>.findCycles(): List<List<String>> {
+    val (parent, neighbors) = entries.toList().first()
+    val node = neighbors.first()
+    return buildList { dfsCycle(node = node, parent = parent, cycles = this) }
+}
+
 private fun Map<String, Set<String>>.dfsCycle(
     node: String,
     parent: String,
-    colors: MutableMap<String, Int> = mutableMapOf(),
-    cycles: MutableList<List<String>> = mutableListOf(),
+    cycles: MutableList<List<String>>,
+    visitCount: MutableMap<String, Int> = mutableMapOf(),
     parents: MutableMap<String, String> = mutableMapOf(),
 ) {
-    val color = colors[node]
-    if (color == 2) {
-        return
-    }
-
-    if (color == 1) {
-        val cycle = mutableListOf<String>()
-        var cur = parent
-        cycle.add(parent)
-
-        while (cur != node) {
-            cur = parents.getValue(cur)
-            cycle.add(cur)
+    when (visitCount[node]) {
+        1 -> {
+            val cycle = buildList {
+                add(parent)
+                var current = parent
+                while (current != node) {
+                    current = parents.getValue(current)
+                    add(current)
+                }
+            }
+            cycles.add(cycle)
         }
-        cycles.add(cycle)
-        return
-    }
 
-    parents[node] = parent
-    colors[node] = 1
+        2 -> Unit
 
-    val neighbors = getValue(node)
-    for (neighbor in neighbors) {
-        if (neighbor == parents[node]) {
-            continue
+        else -> {
+            parents[node] = parent
+            visitCount[node] = 1
+
+            val neighbors = getValue(node)
+            for (neighbor in neighbors) {
+                if (neighbor == parents[node]) {
+                    continue
+                }
+                dfsCycle(neighbor, node, cycles, visitCount, parents)
+            }
+
+            visitCount[node] = 2
         }
-        dfsCycle(neighbor, node, colors, cycles, parents)
     }
-
-    colors[node] = 2
 }
 
-private fun Map<String, Set<String>>.countCyclesOfThree(node: String): Set<List<String>> {
-    val others = getValue(node)
-    var cycles = mutableSetOf<List<String>>()
-    for (a in others) {
-        val aOthers = getValue(a)
-        for (b in others) {
-            if (a == b) continue
-            if (b in aOthers) {
-                cycles.add(listOf(node, a, b).sorted())
+private fun Map<String, Set<String>>.countCyclesOfThree(node: String) =
+    buildSet {
+        val others = getValue(node)
+        for (a in others) {
+            val aNeighbors = getValue(a)
+            for (b in others) {
+                if (a == b) continue
+                if (b in aNeighbors) add(listOf(node, a, b).sorted())
             }
         }
     }
-    return cycles
-}
+
+private fun buildConnectionsMap(input: Input): Map<String, Set<String>> =
+    input.lines
+        .flatMap { it.split('-').let { (a, b) -> listOf(a to b, b to a) } }
+        .distinct()
+        .groupBy { it.first }
+        .mapValues { (_, value) -> value.map { it.second }.toSet() }
